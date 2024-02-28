aec83e6979f67791162c55f244a78b97d7b0e328
okachimachi0522 <okachimachi0522@gmail.com>
2024-02-28 14:54:29 +0900

    main v3

--- practice/src/main/java/com/izakaya/website/service/UserService.java
+++ practice/src/main/java/com/izakaya/website/service/UserService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.izakaya.website.entity.UserEntity;
+import com.izakaya.website.practice.entity.UserEntity;
 import com.izakaya.website.model.User;
 import com.izakaya.website.repository.UserRepository;
 
@@ -20,105 +20,85 @@ import lombok.extern.slf4j.Slf4j;
 public class UserService {
 	
 	private final UserRepository userrepository;
-	import java.util.ArrayList;
-	import java.util.List;
-	import java.util.Optional;
-
-	import org.springframework.stereotype.Service;
-
-	import com.izakaya.website.entity.UserEntity;
-	import com.izakaya.website.model.User;
-	import com.izakaya.website.repository.UserRepository;
-
-	import jakarta.transaction.Transactional;
-	import lombok.RequiredArgsConstructor;
-	import lombok.extern.slf4j.Slf4j;
-
-	@Slf4j
-	@RequiredArgsConstructor
-	@Service
-	public class UserService {
+	
+	/*회원가입*/
+	@Transactional
+	public void create(User user) {
+		UserEntity userEntity = UserEntity.builder()
+				.id(user.getId())
+				.email(user.getEmail())
+				.password(user.getPassword())
+				.name(user.getName())
+				.tel(user.getTel())
+				.build();
+		userrepository.save(userEntity);
+	}
+	
+	/*로그인*/
+	public User login(User user) {
+		Optional<UserEntity> byuserEmail = userrepository.findByEmail(user.getEmail());
 		
-		private final UserRepository userrepository;
-		
-		/*회원가입*/
-		@Transactional
-		public void create(User user) {
-			UserEntity userEntity = UserEntity.builder()
-					.id(user.getId())
-					.email(user.getEmail())
-					.password(user.getPassword())
-					.name(user.getName())
-					.tel(user.getTel())
-					.build();
-			userrepository.save(userEntity);
-		}
-		
-		/*로그인*/
-		public User login(User user) {
-			Optional<UserEntity> byuserEmail = userrepository.findByEmail(user.getEmail());
-			
-			if(byuserEmail.isPresent()) {
-				UserEntity userEntity = byuserEmail.get();
-				if(userEntity.getPassword().equals(user.getPassword())) {
-					return userEntity.toUser();
-				} else {
-					return null;
-				}
+		if(byuserEmail.isPresent()) {
+			UserEntity userEntity = byuserEmail.get();
+			if(userEntity.getPassword().equals(user.getPassword())) {
+				return userEntity.toUser();
 			} else {
 				return null;
 			}
-		}
-		
-		/*회원목록*/
-		public List<User> findAll() {
-			List<UserEntity> userEntityList = userrepository.findAll();
-			List<User> userList = new ArrayList<>();
-			for(UserEntity userEntity : userEntityList) {
-				userList.add(userEntity.toUser());
-			}
-			return userList;
-		}
-		
-		/*회원조회*/
-		public User findById(Long id) {
-			Optional<UserEntity> userEntity = userrepository.findById(id);
-			
-			if(userEntity.isPresent()) {
-				return userEntity.get().toUser();
-			}
+		} else {
 			return null;
 		}
-		
-		/*이메일로 회원조회(회원가입 아이디 중복방지)*/
-		public User findByEmail(String email){
-			Optional<UserEntity> userEntity = userrepository.findByEmail(email);
-			
-			if(userEntity.isPresent()) {
-				return userEntity.get().toUser();
-			}
-			return null;
-		}
-		
-		/*회원정보수정*/
-		@Transactional
-		public void update(User user) {
-			Optional<UserEntity> byId = userrepository.findById(user.getId());
-			
-			if(byId.isPresent()) {
-				UserEntity userEntity = byId.get();
-				userEntity.update(user.getId(),
-								  user.getEmail(),
-								  user.getPassword(), 
-								  user.getName(), 
-								  user.getTel());
-			}
-		} 
-
-		/*회원탈퇴*/
-		@Transactional
-		public void deleteById(Long id) {
-			userrepository.deleteById(id);
-		}
-
 	}
+	
+	/*회원목록*/
+	public List<User> findAll() {
+		List<UserEntity> userEntityList = userrepository.findAll();
+		List<User> userList = new ArrayList<>();
+		for(UserEntity userEntity : userEntityList) {
+			userList.add(userEntity.toUser());
+		}
+		return userList;
+	}
+	
+	/*회원조회*/
+	public User findById(Long id) {
+		Optional<UserEntity> userEntity = userrepository.findById(id);
+		
+		if(userEntity.isPresent()) {
+			return userEntity.get().toUser();
+		}
+		return null;
+	}
+	
+	/*이메일로 회원조회(회원가입 아이디 중복방지)*/
+	public User findByEmail(String email){
+		Optional<UserEntity> userEntity = userrepository.findByEmail(email);
+		
+		if(userEntity.isPresent()) {
+			return userEntity.get().toUser();
+		}
+		return null;
+	}
+	
+	/*회원정보수정*/
+	@Transactional
+	public void update(User user) {
+		Optional<UserEntity> byId = userrepository.findById(user.getId());
+		
+		if(byId.isPresent()) {
+			UserEntity userEntity = byId.get();
+			userEntity.update(user.getId(),
+							  user.getEmail(),
+							  user.getPassword(), 
+							  user.getName(), 
+							  user.getTel());
+		}
+	} 
+
+	/*회원탈퇴*/
+	@Transactional
+	public void deleteById(Long id) {
+		userrepository.deleteById(id);
+	}
+
+}
